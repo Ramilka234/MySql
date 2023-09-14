00ed70bf15703095e6166bd7f739cc627768481b
ramil mukhtarov <ramilasus@gmail.com>
2023-09-15 00:20:41 +0300

    script

---
+++ Script.sql
@@ -0,0 +1,46 @@
+
+CREATE DATABASE IF NOT EXISTS bookstore;
+
+
+USE bookstore;
+
+
+CREATE TABLE IF NOT EXISTS category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS book (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(255),
+    price DECIMAL(10, 2) NOT NULL,
+    cat_id INT,
+    FOREIGN KEY (cat_id) REFERENCES category(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS profile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    birthday VARCHAR(255),
+    user_id INT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS user_has_book (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (book_id) REFERENCES book(id)
+);
